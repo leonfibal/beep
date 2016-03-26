1cb825a771f5432319c7fcb03690d9676a980ac2
Leon Zawodowiec <Leon Zawodowiec>
2016-03-26 10:13:40 +0100

    solution and console project

---
+++ src/Beep/Program.cs
@@ -0,0 +1,13 @@
+﻿using static System.Console;
+
+namespace Beep
+{
+    class Program
+    {
+        static void Main()
+        {
+            WriteLine("Hello world!");
+            ReadKey();
+        }
+    }
+}
